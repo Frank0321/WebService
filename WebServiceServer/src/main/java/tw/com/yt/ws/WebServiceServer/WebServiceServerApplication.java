f0e74dd910523a7a1aad0a3e350a387cc9a9748d
frankchang <frankchang@hitrust.com.tw>
2025-05-03 22:40:54 +0800

    [新增] 新增 server 專案，新增 xml 物件

---
+++ WebServiceServer/src/main/java/tw/com/yt/ws/WebServiceServer/WebServiceServerApplication.java
@@ -0,0 +1,13 @@
+package tw.com.yt.ws.WebServiceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceServerApplication.class, args);
+	}
+
+}
