f4d27a325efcce470c80ac1a61c0212f19b3e2ae
frankchang <frankchang@hitrust.com.tw>
2025-05-04 11:07:00 +0800

    [建立] 建立 client 專案

---
+++ WebServiceClient/src/test/java/tw/com/yt/ws/WebServiceClient/WebServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.yt.ws.WebServiceClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
